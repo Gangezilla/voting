4a996917a3abc253002a93242fccb3b922742661
Scott <scottgangemi@gmail.com>
2016-07-12 21:07:22 +1000

    setting up files and heroku for hosting

---
+++ app.js
@@ -0,0 +1,13 @@
+var express=require('express');
+var mongodb=require('mongodb');
+
+var app=express();
+var MongoClient = mongodb.MongoClient;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/templates');
+
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'), function() {
+    console.log("Listening on " + app.get('port') + ".");
+});
